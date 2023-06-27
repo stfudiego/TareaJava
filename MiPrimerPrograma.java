2565587dcebc1e667e6e4478ff3dbdec833cb8a3
stfudiego <stfudiegomv@gmail.com>
2023-06-27 00:31:29 -0500

    Tarea

---
+++ MiPrimerPrograma.java
@@ -0,0 +1,11 @@
+/**
+ * MiPrimerPrograma
+ */
+public class MiPrimerPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mi nombre es Diego");
+        System.out.println("Tengo 20 años de edad.");
+        System.out.println("Mi ciudad es Lima, PE");
+    }
+}
